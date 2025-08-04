2ce0914085077d2a6de42df4e76f46a36a655e11
Lorenzo <lore.mail.gl@gmail.com>
2025-08-04 03:12:29 +0200

    16 commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
-import ThankYou from './ThankYou'; // Assicurati che questo componente esista
 import PurchRedirect from './components/PurchRedirect';
 
 function App() {
